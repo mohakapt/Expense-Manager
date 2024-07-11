7f33a803b751bed694a96f4bc6651e54ddc660e9
Haytham KATBY <mohakapt@gmail.com>
2024-07-11 18:18:23 +0300

    Initial Commit

---
+++ ExpenseManager/ExpenseManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseManagerApp.swift
+//  ExpenseManager
+//
+//  Created by Heysem Katibi on 11.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
